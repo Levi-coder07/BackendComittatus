824b76157a7ec746a8b13769c722eccaaf8ea7c1
MoisesCA-73 <66378253+MoisesCA-73@users.noreply.github.com>
2021-06-30 19:44:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,20 @@ from flask import render_template
 from flask_cors import CORS, cross_origin # para que no genere errores de CORS al hacer peticiones
 
 from backend.blueprints.task_blueprint import task_blueprint
+from backend.blueprints.usuarios_blueprint import usuarios_blueprint
+from backend.blueprints.login_blueprint import login_blueprint
+from backend.blueprints.curso_del_estudiante_blueprint import curso_del_estudiante_blueprint
+from backend.blueprints.escuela_blueprint import escuela_blueprint
 
-#app = Flask(__name__)
-# para que utilice vue compilado ( npm run build ). En la carpeta dist, esta lo compilado de vue
-app = Flask(__name__,
-            static_folder = "./frontend/dist/static",
-            template_folder = "./frontend/dist")
+app = Flask(__name__)
 
 app.register_blueprint(task_blueprint)
+app.register_blueprint(usuarios_blueprint)
+app.register_blueprint(login_blueprint)
+app.register_blueprint(curso_del_estudiante_blueprint)
+app.register_blueprint(escuela_blueprint)
 
 cors = CORS(app)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def dender_vue(path):
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
